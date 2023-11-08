67a5f552f7cc810a66e3eb96ef5d71b17d766964
aqibrafique <aqibrafique97@gmail.com>
2023-11-08 17:58:59 +0500

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,102 @@
-
-import { useState,useEffect } from 'react'
-import './App.css'
-import Result from './Components/Result'
-import axios from 'axios'
+import { useState, useEffect, useCallback } from "react";
+import "./App.css";
+import Result from "./Components/Result";
+import axios from "axios";
 function App() {
   const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState('')
-  const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-  const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-  const getAllMovies = ( ) =>{
-    axios.get(APIURL)
-    .then(
-      (response) =>{
-        console.log(response.data.results)
-        setMovies(response.data.results)
-      }
-    )
-    .catch(
-      (error) => {console.log(error)}
-    )
-  }
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalpage, settotalpage] = useState(1);
+  const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
+  const APIpage = APIURL + page;
+  const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+  const SAPI = SEARCHAPI + search + "&page=" + page;
 
-  const Changethesearch = (event:any) =>{
-    setSearch(event.target.value)
-    
-  }
-  const getSearchMovies = () =>{
-    axios.get(SEARCHAPI+search)
-    .then(
-      (response) =>{
-        console.log(response.data.results)
-        setMovies(response.data.results)
-      }
-    )
-    .catch(
-      (error) => {console.log(error)}
-    )
-  }
+  const Changethesearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+    setPage(1);
+  };
+
+  const getAllMovies = useCallback(() => {
+    axios
+      .get(APIpage)
+      .then((response) => {
+        console.log(response.data);
+        settotalpage(response.data.total_pages);
+        setPage(response.data.page);
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [APIpage, settotalpage, setPage, setMovies]);
+
+  const GetSearchMovies = useCallback(() => {
+    console.log(SAPI);
+    axios
+      .get(SAPI)
+      .then((response) => {
+        console.log(response.data);
+        settotalpage(response.data.total_pages);
+        setPage(response.data.page);
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [SAPI]);
+
   useEffect(() => {
-    if(search===""){
-      getAllMovies()
-    }else{
-      getSearchMovies()
+    if (search === "") {
+      getAllMovies();
+    } else {
+      GetSearchMovies();
     }
-  }, [search])
-  
+  }, [search, page, getAllMovies, GetSearchMovies]);
+
+  const PrevMovie = () => {
+    if (page !== 1) {
+      setPage(page - 1);
+    }
+  };
+  const NextMovie = () => {
+    if (page !== totalpage) {
+      setPage(page + 1);
+    }
+  };
 
   return (
     <>
-      <div className='container mx-auto border  p-4'>
-        <div className='flex flex-col gap-10 '>
-        <label className='font-bold text-2xl '>Search Movies</label>
-        <input type='text' value={search} onChange={Changethesearch} className='border mx-auto border-slate-600 rounded-2xl w-1/2  p-3'/></div>
-        <Result movies={movies}/>
+      <div className="container mx-auto border  p-4">
+        <div className="flex flex-col gap-10 ">
+          <label className="font-bold text-3xl text-yellow-400 ">Search Movies</label>
+          <input
+            type="text"
+            value={search}
+            onChange={Changethesearch}
+            className="border-2 border-yellow-400 mx-auto  rounded-lg md:w-3/4 w-full  p-3 "
+            
+          />
+        </div>
+        <Result movies={movies} />
+      </div>
+      <div className="flex mt-5 justify-center">
+        <div className=" w-full p-4 flex flex-row items-center justify-around md:w-1/2">
+          <button className="text-3xl font-semibold" onClick={PrevMovie}>
+            Prev
+          </button>
+          <span className="text-3xl text-slate-500">
+            {page} of {totalpage}
+          </span>
+          <button className="text-3xl font-semibold" onClick={NextMovie}>
+            Next
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,34 +1,48 @@
-import React from "react";
 
-function Result(props:any) {
+interface ResultProps {
+  movies: {
+    poster_path: string;
+    original_title: string;
+    vote_average: number;
+  }[];
+}
+interface BoxProps {
+  image: string;
+  title: string;
+  rating: number;
+}
+function Result(props: ResultProps) {
+  const box = props.movies.map((items ,index) => {
+    return (
+      <Box
+        key={index}
+        image={items.poster_path}
+        title={items.original_title}
+        rating={items.vote_average}
+      />
+    );
+  });
 
-        const box = props.movies.map(
-                (items:any, index:any) =>{
-                        return(
-                               <Box key={index} image={items.poster_path} title={items.original_title} rating={items.vote_average}/> 
-                        )
-                }
-        )
-        
   return (
-    <div className="w-full grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mt-8  gap-4">
+    <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mt-8  gap-4">
       {box}
-      
     </div>
   );
 }
 
 export default Result;
 
-
-const Box = (props:any) => {
-        const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-        return(
-<div className=" w-full  flex flex-col">
-        <img src={IMGPATH+props.image} />
-        <div className="flex flex-row justify-between px-2 bg-slate-600 items-center py-6">
-          <span className="font-bold text-xl text-white">{props.title}</span>
-          <span className="text-3xl font-medium text-yellow-400 ">{props.rating}</span>
-        </div>
-      </div>)
-}
+const Box = (props: BoxProps) => {
+  const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+  return (
+    <div className=" w-full  grid  border shadow-lg">
+      <img className="object-cover" src={IMGPATH + props.image} />
+      <div className="flex flex-row justify-between px-2  items-center py-6">
+        <span className="font-bold w-1/2 text-xl ">{props.title}</span>
+        <span className="text-3xl w-1/2 font-medium text-yellow-400 ">
+          {props.rating}
+        </span>
+      </div>
+    </div>
+  );
+};
